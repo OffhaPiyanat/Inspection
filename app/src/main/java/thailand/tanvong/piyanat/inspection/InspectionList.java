0109488521e51c83c08d431528df748a36f0b341
OFFHA <offha@hotmail.com>
2016-03-20 17:06:50 +0700

    Create Cursor Where from Catagory finish

--- app/src/main/java/thailand/tanvong/piyanat/inspection/InspectionList.java
+++ app/src/main/java/thailand/tanvong/piyanat/inspection/InspectionList.java
@@ -1,7 +1,10 @@
 package thailand.tanvong.piyanat.inspection;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,15 +30,36 @@ public class InspectionList extends AppCompatActivity {
         //Show View
         showView();
 
+        //Create ListView
+        createListview();
+
+
+    }   //Main Method
+
+    private void createListview() {
+
+        //Read Data From SQLite
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " +
+                MyManage.Inspection_table + " WHERE " +
+                MyManage.column_Catagory + " = " + "'" + chooseCategoryString + "'", null);
+
+        cursor.moveToFirst();
+        int intCount = cursor.getCount();
+        Log.d("Spect", "intCount ==> " + intCount);
+
+
+    }   // createListView
+
+    private void showView() {
+
         chooseCategoryString = getIntent().getStringExtra("Title");
-        chooseIconAnInt = getIntent().getIntExtra("Icon",R.drawable.build3);
+        chooseIconAnInt = getIntent().getIntExtra("Icon", R.drawable.build3);
 
         titleTextView.setText(chooseCategoryString);
         iconImageView.setImageResource(chooseIconAnInt);
 
-    }   //Main Method
-
-    private void showView() {
 
     }
 
